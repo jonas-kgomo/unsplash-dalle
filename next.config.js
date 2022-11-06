863938599a6c7494c163e5c2eb5a08319014af58
jonas-kgomo <jonaskgmoo@gmail.com>
2022-11-06 03:12:26 +0000

    api addition

--- next.config.js
+++ next.config.js
@@ -2,4 +2,11 @@ module.exports = {
   images: {
     domains: ['store.storeimages.cdn-apple.com'],
   },
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, ...{ topLevelAwait: true }};
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true 
+    return config;
+  },
 }
